720da7a4becaf734f2a9731393f92c45f73cc419
Matteo Collina <hello@matteocollina.com>
2015-04-06 11:13:51 +0200

    Added subscriptions-handling methods to persistence.

--- test/abstract-persistence.js
+++ test/abstract-persistence.js
@@ -26,6 +26,83 @@ function abstractPersistence(opts) {
       }))
     })
   })
+
+  test('store and look up subscriptions by client', function(t) {
+    var instance  = persistence()
+      , clientId  = 'abcde'
+      , subs      = [{
+            topic: 'hello'
+          , qos: 1
+        },  {
+            topic: 'matteo'
+          , qos: 1
+        }]
+
+    instance.persistSubscriptions(clientId, subs, function(err) {
+      t.notOk(err, 'no error')
+      instance.subscriptionsByClient(clientId, function(err, resubs) {
+        t.notOk(err, 'no error')
+        t.deepEqual(resubs, subs)
+        instance.destroy(t.end.bind(t))
+      })
+    })
+  })
+
+  test('store and look up subscriptions by pattern', function(t) {
+    var instance  = persistence()
+      , clientId  = 'abcde'
+      , subs      = [{
+            topic: 'hello'
+          , qos: 1
+        },  {
+            topic: 'matteo'
+          , qos: 1
+        }]
+
+    instance.persistSubscriptions(clientId, subs, function(err) {
+      t.notOk(err, 'no error')
+      var stream = instance.subscriptionsByPattern('hello')
+
+      stream.pipe(concat(function(resubs) {
+        t.notOk(err, 'no error')
+        t.deepEqual(resubs, [{
+            clientId: clientId
+          , topic: 'hello'
+          , qos: 1
+        }])
+        instance.destroy(t.end.bind(t))
+      }))
+    })
+  })
+
+  test('clean subscriptions', function(t) {
+    var instance  = persistence()
+      , clientId  = 'abcde'
+      , subs      = [{
+            topic: 'hello'
+          , qos: 1
+        },  {
+            topic: 'matteo'
+          , qos: 1
+        }]
+
+    instance.persistSubscriptions(clientId, subs, function(err) {
+      t.notOk(err, 'no error')
+      instance.cleanSubscriptions(clientId, function(err) {
+        t.notOk(err, 'no error')
+        var stream = instance.subscriptionsByPattern('hello')
+
+        stream.pipe(concat(function(resubs) {
+          t.deepEqual(resubs, [], 'no subscriptions')
+
+          instance.subscriptionsByClient(clientId, function(err, resubs) {
+            t.deepEqual(resubs, [], 'no subscriptions')
+            instance.destroy(t.end.bind(t))
+          })
+        }))
+      })
+    })
+  })
 }
 
 module.exports = abstractPersistence
