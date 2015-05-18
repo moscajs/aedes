c87b41bf488f5eea434af506f13019db52c359fe
Matteo Collina <hello@matteocollina.com>
2015-05-19 01:40:33 +0200

    Standardized test/qos2

--- test/qos2.js
+++ test/qos2.js
@@ -1,37 +1,34 @@
+var test = require('tape').test
+var helper = require('./helper')
+var setup = helper.setup
+var connect = helper.connect
 
-var test      = require('tape').test
-  , helper    = require('./helper')
-  , aedes     = require('../')
-  , setup     = helper.setup
-  , connect   = helper.connect
-  , noError   = helper.noError
-
-test.skip('publish QoS 2', function(t) {
-  var s         = connect(setup())
+test.skip('publish QoS 2', function (t) {
+  var s = connect(setup())
 
   s.inStream.write({
-      cmd: 'publish'
-    , topic: 'hello'
-    , payload: 'world'
-    , qos: 2
-    , messageId: 42
+    cmd: 'publish',
+    topic: 'hello',
+    payload: 'world',
+    qos: 2,
+    messageId: 42
   })
 
-  s.outStream.once('data', function(packet) {
+  s.outStream.once('data', function (packet) {
     t.deepEqual(packet, {
-        cmd: 'pubrec'
-      , messageId: 42
+      cmd: 'pubrec',
+      messageId: 42
     }, 'pubrec must match')
 
     s.inStream.write({
-        cmd: 'pubrel'
-      , messageId: 42
+      cmd: 'pubrel',
+      messageId: 42
     })
 
-    s.outStream.once('data', function(packet) {
+    s.outStream.once('data', function (packet) {
       t.deepEqual(packet, {
-          cmd: 'pubcomp'
-        , messageId: 42
+        cmd: 'pubcomp',
+        messageId: 42
       }, 'pubcomp must match')
 
       t.end()
