2b1c263d9e71a409942cb152adf125be2afe7d8d
Matteo Collina <hello@matteocollina.com>
2016-09-27 22:47:38 +0200

    Added websocket example. Updated dependencies.

--- example.js
+++ example.js
@@ -2,10 +2,21 @@
 
 var aedes = require('./aedes')()
 var server = require('net').createServer(aedes.handle)
+var httpServer = require('http').createServer()
+var ws = require('websocket-stream')
 var port = 1883
+var wsPort = 8888
 
 server.listen(port, function () {
   console.log('server listening on port', port)
+})
+
+ws.createServer({
+  server: httpServer
+}, aedes.handle)
+
+httpServer.listen(wsPort, function () {
+  console.log('websocket server listening on port', wsPort)
 })
 
 aedes.on('clientError', function (client, err) {
