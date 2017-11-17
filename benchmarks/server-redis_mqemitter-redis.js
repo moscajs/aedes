dbaa89da895311fda0898a40616102c6b0f8c0ad
cordovapolymer <cordovapolymer@grr.la>
2017-11-17 06:16:17 -0500

    added benchmark server for tests with aedes-persistence-redis and mqemitter-redis

---
+++ benchmarks/server-redis_mqemitter-redis.js
@@ -0,0 +1,41 @@
+'use strict'
+
+// To be used with cpuprofilify http://npm.im/cpuprofilify
+var aedesPersistenceRedis = require('aedes-persistence-redis')({
+  port: 6379,          // Redis port
+  host: '127.0.0.1',   // Redis host
+  family: 4,           // 4 (IPv4) or 6 (IPv6)
+  password: 'auth',
+  db: 0,
+  maxSessionDelivery: 100 // maximum offline messages deliverable on client CONNECT, default is 1000
+})
+var mq = require('mqemitter-redis')({
+  port: 6379,          // Redis port
+  host: '127.0.0.1',   // Redis host
+  family: 4,           // 4 (IPv4) or 6 (IPv6)
+  password: 'auth',
+  db: 0,
+  maxSessionDelivery: 100 // maximum offline messages deliverable on client CONNECT, default is 1000
+})
+
+var aedes = require('../')({persistence: aedesPersistenceRedis, concurrency: 0})
+var server = require('net').createServer(aedes.handle)
+var port = 1883
+
+server.listen(port, function () {
+  console.error('server listening on port', port, 'pid', process.pid)
+})
+
+aedes.on('clientError', function (client, err) {
+  console.error('client error', client.id, err.message)
+})
+
+// Cleanly shut down process on SIGTERM to ensure that perf-<pid>.map gets flushed
+process.on('SIGINT', onSIGINT)
+
+function onSIGINT () {
+  // IMPORTANT to log on stderr, to not clutter stdout which is purely for data, i.e. dtrace stacks
+  console.error('Caught SIGTERM, shutting down.')
+  server.close()
+  process.exit(0)
+}
