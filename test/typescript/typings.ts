1d90559d077f76e6c8c2a709658e876d27ace217
Rodrigo Saboya <saboya@users.noreply.github.com>
2018-05-06 17:26:33 -0300

    Adding Typescript typings.

---
+++ test/typescript/typings.ts
@@ -0,0 +1,98 @@
+// relative path uses package.json {"types":"types/index.d.ts", ...}
+
+import Aedes = require ('../..')
+import { IPublishPacket, ISubscribePacket, ISubscription, IUnsubscribePacket } from 'mqtt-packet'
+import { createServer } from 'net'
+
+const aedes = Aedes({
+  concurrency: 100,
+  heartbeatInterval: 60000,
+  connectTimeout: 30000,
+  authenticate: (client, username: string, password: string, callback) => {
+    if (username === 'test' && password === 'test') {
+      callback(null, true)
+    } else {
+      const error = new Error() as Error & { returnCode: number }
+      error.returnCode = 1
+
+      callback(error, false)
+    }
+  },
+  authorizePublish: (client, packet: IPublishPacket, callback) => {
+    if (packet.topic === 'aaaa') {
+      return callback(new Error('wrong topic'))
+    }
+
+    if (packet.topic === 'bbb') {
+      packet.payload = new Buffer('overwrite packet payload')
+    }
+
+    callback(null)
+  },
+  authorizeSubscribe: (client, sub: ISubscription, callback) => {
+    if (sub.topic === 'aaaa') {
+      return callback(new Error('wrong topic'))
+    }
+
+    if (sub.topic === 'bbb') {
+      // overwrites subscription
+      sub.qos = 2
+    }
+
+    callback(null, sub)
+  },
+  authorizeForward: (client, packet: IPublishPacket) => {
+    if (packet.topic === 'aaaa' && client.id === 'I should not see this') {
+      return null
+      // also works with return undefined
+    } else if (packet.topic === 'aaaa' && client.id === 'I should not see this either') {
+      return
+    }
+
+    if (packet.topic === 'bbb') {
+      packet.payload = new Buffer('overwrite packet payload')
+    }
+
+    return packet
+  }
+})
+
+const server = createServer(aedes.handle)
+
+aedes.on('client', client => {
+  console.log(`client: ${client.id} connected`)
+})
+
+aedes.on('clientDisconnect', client => {
+  console.log(`client: ${client.id} disconnected`)
+})
+
+aedes.on('keepaliveTimeout', client => {
+  console.log(`client: ${client.id} timed out`)
+})
+
+aedes.on('clientError', client => {
+  console.log(`client: ${client.id} error`)
+})
+
+aedes.on('connectionError', client => {
+  console.log('connectionError')
+})
+
+aedes.subscribe('aaaa', (packet: ISubscribePacket, cb) => {
+  console.log('cmd')
+  console.log(packet.subscriptions)
+  cb()
+}, () => {
+  console.log('done subscribing')
+})
+
+aedes.unsubscribe('aaaa', (packet: IUnsubscribePacket, cb) => {
+  console.log('cmd')
+  console.log(packet.unsubscriptions)
+  cb()
+}, () => {
+  console.log('done unsubscribing')
+})
+
+aedes.close()
---
+++ types/index.d.ts
@@ -0,0 +1,82 @@
+/// <reference types="node" />
+
+import { IPublishPacket, ISubscribePacket, ISubscription, IUnsubscribePacket } from 'mqtt-packet'
+import { Duplex } from 'stream'
+import EventEmitter = NodeJS.EventEmitter
+
+declare enum AuthErrorCode {
+  UNNACCEPTABLE_PROTOCOL = 1,
+  IDENTIFIER_REJECTED = 2,
+  SERVER_UNAVAILABLE = 3,
+  BAD_USERNAME_OR_PASSWORD = 4
+}
+
+interface Client extends EventEmitter {
+  id: string
+  clean: boolean
+
+  on (event: 'error', cb: (err: Error) => void): this
+
+  publish (message: IPublishPacket, callback?: () => void): void
+  subscribe (
+    subscriptions: ISubscription | ISubscription[] | ISubscribePacket,
+    callback?: () => void
+  ): void
+  unsubscribe (topicObjects: ISubscription | ISubscription[], callback?: () => void): void
+  close (callback?: () => void): void
+}
+
+type AuthenticateCallback = (
+  client: Client,
+  username: string,
+  password: string,
+  done: (err: Error & { returnCode: AuthErrorCode } | null, success: boolean | null) => void
+) => void
+
+type AuthorizePublishCallback = (client: Client, packet: IPublishPacket, done: (err?: Error | null) => void) => void
+
+type AuthorizeSubscribeCallback = (client: Client, subscription: ISubscription, done: (err: Error | null, subscription?: ISubscription | null) => void) => void
+
+type AuthorizeForwardCallback = (client: Client, packet: IPublishPacket) => IPublishPacket | null | void
+
+type PublishedCallback = (packet: IPublishPacket, client: Client, done: () => void) => void
+
+interface AedesOptions {
+  mq?: any
+  persistence?: any
+  concurrency?: number
+  heartbeatInterval?: number
+  connectTimeout?: number
+  authenticate?: AuthenticateCallback
+  authorizePublish?: AuthorizePublishCallback
+  authorizeSubscribe?: AuthorizeSubscribeCallback
+  authorizeForward?: AuthorizeForwardCallback
+  published?: PublishedCallback
+}
+
+interface Aedes extends EventEmitter {
+  handle: (stream: Duplex) => void
+
+  authenticate: AuthenticateCallback
+  authorizePublish: AuthorizePublishCallback
+  authorizeSubscribe: AuthorizeSubscribeCallback
+  authorizeForward: AuthorizeForwardCallback
+  published: PublishedCallback
+
+  on (event: 'client' | 'clientDisconnect' | 'keepaliveTimeout', cb: (client: Client) => void): this
+  on (event: 'clientError' | 'connectionError', cb: (client: Client, error: Error) => void): this
+  on (event: 'ping', cb: (packet: any, client: Client) => void): this
+
+  publish (packet: IPublishPacket & { topic: string | Buffer }, done: () => void): void
+  subscribe (topic: string, callback: (packet: ISubscribePacket, cb: () => void) => void, done: () => void): void
+  unsubscribe (
+    topic: string,
+    callback: (packet: IUnsubscribePacket, cb: () => void) => void,
+    done: () => void
+  ): void
+  close (callback?: () => void): void
+}
+
+declare function aedes (options?: AedesOptions): Aedes
+
+export = aedes
