2cee5d169a5571537e2775ea681cc34bda211da5
gnought <1684105+gnought@users.noreply.github.com>
2019-07-20 22:04:45 +0800

    More reliable regr-21 test if we use external mq

--- test/regr-21.js
+++ test/regr-21.js
@@ -19,14 +19,15 @@ test('after an error, outstanding packets are discarded', function (t) {
   s.broker.mq.on('hello', function (msg, cb) {
     t.pass('first msg received')
     s.inStream.destroy(new Error('something went wrong'))
-    setImmediate(cb)
-  })
-
-  process.nextTick(function () {
-    s.inStream.write(packet)
-    setImmediate(function () {
+    cb()
+    setImmediate(() => {
+      packet.topic = 'foo'
       s.inStream.write(packet)
       s.inStream.write(packet)
     })
   })
+  s.broker.mq.on('foo', function (msg, cb) {
+    t.fail('msg received')
+  })
+  s.inStream.write(packet)
 })
