7ac0234839cfb7d0387b69d67dabcb1f3fe4ec54
Renan Duarte <38355185+r3na@users.noreply.github.com>
2021-04-19 10:39:08 +0200

    chore(types): include types/packet in aedes.d.ts (#616)
    missing the "export * from './types/packet"  in order to have packet types available (typescript)

--- aedes.d.ts
+++ aedes.d.ts
@@ -4,5 +4,6 @@ export declare function aedes (options?: AedesOptions): Aedes
 export declare function Server (options?: AedesOptions): Aedes
 
 export * from './types/instance'
+export * from './types/packet'
 export * from './types/client'
 export default aedes
