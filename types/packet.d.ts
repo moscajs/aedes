d6749427f05a22ba1df8538fc0a5df808c7d5b5c
Hans Klunder <hans.klunder@gmail.com>
2022-05-12 20:21:58 +0200

    feature: add ClientId type definition

--- types/packet.d.ts
+++ types/packet.d.ts
@@ -1,9 +1,10 @@
 import { AedesPacket } from 'aedes-packet'
 import { IConnackPacket, IConnectPacket, IPingreqPacket, IPublishPacket, IPubrelPacket, ISubscribePacket, ISubscription, IUnsubscribePacket } from 'mqtt-packet'
 
+export type ClientId = string
 export type SubscribePacket = ISubscribePacket & { cmd: 'subscribe' }
 export type UnsubscribePacket = IUnsubscribePacket & { cmd: 'unsubscribe' }
-export type Subscription = ISubscription & { clientId?: string }
+export type Subscription = ISubscription & { clientId?: ClientId }
 export type Subscriptions = { subscriptions: Subscription[] }
 
 export type PublishPacket = IPublishPacket & { cmd: 'publish' }
