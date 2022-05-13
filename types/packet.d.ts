40a1a0096effb62f13f0f97f0281d455773e988d
Hans Klunder <hans.klunder@gmail.com>
2022-05-13 16:05:26 +0200

    Remove ClientID type

--- types/packet.d.ts
+++ types/packet.d.ts
@@ -1,10 +1,9 @@
 import { AedesPacket } from 'aedes-packet'
 import { IConnackPacket, IConnectPacket, IPingreqPacket, IPublishPacket, IPubrelPacket, ISubscribePacket, ISubscription, IUnsubscribePacket } from 'mqtt-packet'
 
-export type ClientId = string
 export type SubscribePacket = ISubscribePacket & { cmd: 'subscribe' }
 export type UnsubscribePacket = IUnsubscribePacket & { cmd: 'unsubscribe' }
-export type Subscription = ISubscription & { clientId?: ClientId }
+export type Subscription = ISubscription & { clientId?: string }
 export type Subscriptions = { subscriptions: Subscription[] }
 
 export type PublishPacket = IPublishPacket & { cmd: 'publish' }
