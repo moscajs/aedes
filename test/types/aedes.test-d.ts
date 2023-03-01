deb0e5eb32ea2d1b5be896d6e6f77a46d90bed99
Daniel Lando <daniel.sorridi@gmail.com>
2023-03-01 08:53:28 +0100

    style: fix lint

--- test/types/aedes.test-d.ts
+++ test/types/aedes.test-d.ts
@@ -1,6 +1,6 @@
 
-import { IncomingMessage } from "node:http";
-import { Socket } from "node:net";
+import { IncomingMessage } from 'node:http'
+import { Socket } from 'node:net'
 import type {
   Brokers,
   AuthenticateError,
@@ -9,26 +9,26 @@ import type {
 } from '../../aedes'
 import Aedes, { createServer } from '../../aedes'
 import type { AedesPublishPacket, ConnackPacket, ConnectPacket, PingreqPacket, PublishPacket, PubrelPacket, Subscription, SubscribePacket, UnsubscribePacket } from '../../types/packet'
-import { expectType } from "tsd";
+import { expectType } from 'tsd'
 
 // Aedes server
-let broker = createServer();
-expectType<Aedes>(broker);
+let broker = createServer()
+expectType<Aedes>(broker)
 
 broker = new Aedes({
-  id: "aedes",
+  id: 'aedes',
   concurrency: 100,
   heartbeatInterval: 60000,
   connectTimeout: 30000,
   maxClientsIdLength: 23,
   preConnect: (client: Client, packet: ConnectPacket, callback) => {
     if (client.req) {
-      callback(new Error("not websocket stream"), false);
+      callback(new Error('not websocket stream'), false)
     }
-    if (client.conn instanceof Socket && client.conn.remoteAddress === "::1") {
-      callback(null, true);
+    if (client.conn instanceof Socket && client.conn.remoteAddress === '::1') {
+      callback(null, true)
     } else {
-      callback(new Error("connection error"), false);
+      callback(new Error('connection error'), false)
     }
   },
   authenticate: (
@@ -38,16 +38,16 @@ broker = new Aedes({
     callback
   ) => {
     if (
-      username === "test" &&
-      password === Buffer.from("test") &&
+      username === 'test' &&
+      password === Buffer.from('test') &&
       client.version === 4
     ) {
-      callback(null, true);
+      callback(null, true)
     } else {
-      const error = new Error() as AuthenticateError;
-      error.returnCode = 1;
+      const error = new Error() as AuthenticateError
+      error.returnCode = 1
 
-      callback(error, false);
+      callback(error, false)
     }
   },
   authorizePublish: (
@@ -55,167 +55,167 @@ broker = new Aedes({
     packet: PublishPacket,
     callback
   ) => {
-    if (packet.topic === "aaaa") {
-      return callback(new Error("wrong topic"));
+    if (packet.topic === 'aaaa') {
+      return callback(new Error('wrong topic'))
     }
 
-    if (packet.topic === "bbb") {
-      packet.payload = Buffer.from("overwrite packet payload");
+    if (packet.topic === 'bbb') {
+      packet.payload = Buffer.from('overwrite packet payload')
     }
 
-    callback(null);
+    callback(null)
   },
   authorizeSubscribe: (client: Client, sub: Subscription, callback) => {
-    if (sub.topic === "aaaa") {
-      return callback(new Error("wrong topic"));
+    if (sub.topic === 'aaaa') {
+      return callback(new Error('wrong topic'))
     }
 
-    if (sub.topic === "bbb") {
+    if (sub.topic === 'bbb') {
       // overwrites subscription
-      sub.qos = 2;
+      sub.qos = 2
     }
 
-    callback(null, sub);
+    callback(null, sub)
   },
   authorizeForward: (client: Client, packet: AedesPublishPacket) => {
-    if (packet.topic === "aaaa" && client.id === "I should not see this") {
-      return null;
+    if (packet.topic === 'aaaa' && client.id === 'I should not see this') {
+      return null
       // also works with return undefined
     } else if (
-      packet.topic === "aaaa" &&
-      client.id === "I should not see this either"
+      packet.topic === 'aaaa' &&
+      client.id === 'I should not see this either'
     ) {
-      return;
+      return
     }
 
-    if (packet.topic === "bbb") {
-      packet.payload = Buffer.from("overwrite packet payload");
+    if (packet.topic === 'bbb') {
+      packet.payload = Buffer.from('overwrite packet payload')
     }
 
-    return packet;
+    return packet
   },
   published: (packet: AedesPublishPacket, client: Client, callback) => {
-    callback(null);
-    callback(new Error());
-  },
-});
+    callback(null)
+    callback(new Error())
+  }
+})
 
-expectType<Aedes>(broker);
+expectType<Aedes>(broker)
 
-expectType<Readonly<Brokers>>(broker.brokers);
+expectType<Readonly<Brokers>>(broker.brokers)
 
-expectType<Aedes>(broker.on("closed", () => {}));
-expectType<Aedes>(broker.on("client", (client: Client) => {}));
-expectType<Aedes>(broker.on("clientReady", (client: Client) => {}));
-expectType<Aedes>(broker.on("clientDisconnect", (client: Client) => {}));
-expectType<Aedes>(broker.on("keepaliveTimeout", (client: Client) => {}));
+expectType<Aedes>(broker.on('closed', () => {}))
+expectType<Aedes>(broker.on('client', (client: Client) => {}))
+expectType<Aedes>(broker.on('clientReady', (client: Client) => {}))
+expectType<Aedes>(broker.on('clientDisconnect', (client: Client) => {}))
+expectType<Aedes>(broker.on('keepaliveTimeout', (client: Client) => {}))
 expectType<Aedes>(
-  broker.on("clientError", (client: Client, error: Error) => {})
-);
+  broker.on('clientError', (client: Client, error: Error) => {})
+)
 expectType<Aedes>(
-  broker.on("connectionError", (client: Client, error: Error) => {})
-);
+  broker.on('connectionError', (client: Client, error: Error) => {})
+)
 expectType<Aedes>(
-  broker.on("connackSent", (packet: ConnackPacket, client: Client) => {})
-);
+  broker.on('connackSent', (packet: ConnackPacket, client: Client) => {})
+)
 expectType<Aedes>(
-  broker.on("ping", (packet: PingreqPacket, client: Client) => {})
-);
+  broker.on('ping', (packet: PingreqPacket, client: Client) => {})
+)
 expectType<Aedes>(
   broker.on(
-    "publish",
+    'publish',
     (packet: AedesPublishPacket, client: Client | null) => {}
   )
-);
+)
 expectType<Aedes>(
-  broker.on("ack", (packet: PublishPacket | PubrelPacket, client: Client) => {})
-);
+  broker.on('ack', (packet: PublishPacket | PubrelPacket, client: Client) => {})
+)
 expectType<Aedes>(
-  broker.on("subscribe", (subscriptions: Subscription[], client: Client) => {})
-);
+  broker.on('subscribe', (subscriptions: Subscription[], client: Client) => {})
+)
 expectType<Aedes>(
-  broker.on("unsubscribe", (unsubscriptions: string[], client: Client) => {})
-);
+  broker.on('unsubscribe', (unsubscriptions: string[], client: Client) => {})
+)
 
 expectType<void>(
   broker.publish({} as PublishPacket, (error?: Error) => {
     if (error) {
-      console.error(error);
+      console.error(error)
     }
   })
-);
+)
 
 expectType<void>(
   broker.subscribe(
-    "topic",
+    'topic',
     (packet: AedesPublishPacket, callback: () => void) => {},
     () => {}
   )
-);
+)
 
 expectType<void>(
   broker.unsubscribe(
-    "topic",
+    'topic',
     (packet: AedesPublishPacket, callback: () => void) => {},
     () => {}
   )
-);
+)
 
-expectType<void>(broker.close());
-expectType<void>(broker.close(() => {}));
+expectType<void>(broker.close())
+expectType<void>(broker.close(() => {}))
 
 // Aedes client
-const client = broker.handle({} as Connection, {} as IncomingMessage);
-const client2 = broker.handle({} as Connection);
+const client = broker.handle({} as Connection, {} as IncomingMessage)
+const client2 = broker.handle({} as Connection)
 
-expectType<Client>(client);
-expectType<Client>(client2);
+expectType<Client>(client)
+expectType<Client>(client2)
 
-expectType<Connection>(client.conn);
-expectType<IncomingMessage>(client.req!);
+expectType<Connection>(client.conn)
+expectType<IncomingMessage>(client.req!)
 
-expectType<Client>(client.on("connected", () => {}));
+expectType<Client>(client.on('connected', () => {}))
 expectType<Client>(
-  client.on("error", (error: Error) => {
+  client.on('error', (error: Error) => {
     if (error) {
-      console.error(error);
+      console.error(error)
     }
   })
-);
+)
 
 expectType<void>(
   client.publish({} as PublishPacket, (error?: Error) => {
     if (error) {
-      console.error(error);
+      console.error(error)
     }
   })
-);
-expectType<void>(client.publish({} as PublishPacket));
+)
+expectType<void>(client.publish({} as PublishPacket))
 
 expectType<void>(
   client.subscribe({} as Subscription, (error?: Error) => {
     if (error) {
-      console.error(error);
+      console.error(error)
     }
   })
-);
-expectType<void>(client.subscribe({} as Subscription));
-expectType<void>(client.subscribe([] as Subscription[]));
-expectType<void>(client.subscribe({} as SubscribePacket));
+)
+expectType<void>(client.subscribe({} as Subscription))
+expectType<void>(client.subscribe([] as Subscription[]))
+expectType<void>(client.subscribe({} as SubscribePacket))
 
 expectType<void>(
   client.unsubscribe({} as Subscription, (error?: Error) => {
     if (error) {
-      console.error(error);
+      console.error(error)
     }
   })
-);
-expectType<void>(client.unsubscribe({} as Subscription));
-expectType<void>(client.unsubscribe([] as Subscription[]));
-expectType<void>(client.unsubscribe({} as UnsubscribePacket));
+)
+expectType<void>(client.unsubscribe({} as Subscription))
+expectType<void>(client.unsubscribe([] as Subscription[]))
+expectType<void>(client.unsubscribe({} as UnsubscribePacket))
 
-expectType<void>(client.emptyOutgoingQueue());
-expectType<void>(client.emptyOutgoingQueue(() => {}));
+expectType<void>(client.emptyOutgoingQueue())
+expectType<void>(client.emptyOutgoingQueue(() => {}))
 
-expectType<void>(client.close());
-expectType<void>(client.close(() => {}));
+expectType<void>(client.close())
+expectType<void>(client.close(() => {}))
