bcc5497b1610c498d24ea04656cc7dc033002d06
hjdhjd <hjdhjd@users.noreply.github.com>
2023-11-26 17:31:51 -0600

    Fix module exports for TypeScript for ES6 compliance

--- aedes.d.ts
+++ aedes.d.ts
@@ -1,12 +1,5 @@
-import Aedes, { AedesOptions } from './types/instance'
+export * from './types/instance';
+export * from './types/packet';
+export * from './types/client';
 
-export declare function createBroker (options?: AedesOptions): Aedes
-
-export * from './types/instance'
-export * from './types/packet'
-export * from './types/client'
-export default Aedes
-
-declare module 'aedes' {
-  export = Aedes
-}
+export { default } from './types/instance';
