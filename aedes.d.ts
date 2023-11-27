71708f246ff4412b05f2c3b849e9ca6ab9b46b28
hjdhjd <hjdhjd@users.noreply.github.com>
2023-11-27 12:01:13 -0600

    Provide an explicit export for createBroker.

--- aedes.d.ts
+++ aedes.d.ts
@@ -1,3 +1,7 @@
+import Aedes, { AedesOptions } from './types/instance'
+
+export declare function createBroker(options?: AedesOptions): Aedes;
+
 export * from './types/instance';
 export * from './types/packet';
 export * from './types/client';
