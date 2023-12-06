624263b5e9f1a36b7695d2d24cd391319d32bad5
hjdhjd <hjdhjd@users.noreply.github.com>
2023-12-06 11:07:44 -0600

    Linting

--- aedes.d.ts
+++ aedes.d.ts
@@ -1,9 +1,9 @@
 import Aedes, { AedesOptions } from './types/instance'
 
-export declare function createBroker(options?: AedesOptions): Aedes;
+export declare function createBroker(options?: AedesOptions): Aedes
 
-export * from './types/instance';
-export * from './types/packet';
-export * from './types/client';
+export * from './types/instance'
+export * from './types/packet'
+export * from './types/client'
 
-export { default } from './types/instance';
+export { default } from './types/instance'
