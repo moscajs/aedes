b54da7afd96fa68b94a554303e0ad958ad9640e6
Hans Klunder <hans.klunder@gmail.com>
2025-06-16 23:49:09 +0200

    chore: update dependencies

--- types/packet.d.ts
+++ types/packet.d.ts
@@ -11,17 +11,17 @@ import {
 } from 'mqtt-packet'
 import { Client } from './client'
 
-export type SubscribePacket = ISubscribePacket & { cmd: 'subscribe' };
-export type UnsubscribePacket = IUnsubscribePacket & { cmd: 'unsubscribe' };
-export type Subscription = ISubscription & { clientId?: Client['id'] };
-export type Subscriptions = { subscriptions: Subscription[] };
+export type SubscribePacket = ISubscribePacket & { cmd: 'subscribe' }
+export type UnsubscribePacket = IUnsubscribePacket & { cmd: 'unsubscribe' }
+export type Subscription = ISubscription & { clientId?: Client['id'] }
+export type Subscriptions = { subscriptions: Subscription[] }
 
-export type PublishPacket = IPublishPacket & { cmd: 'publish' };
+export type PublishPacket = IPublishPacket & { cmd: 'publish' }
 
-export type ConnectPacket = IConnectPacket & { cmd: 'connect' };
-export type ConnackPacket = IConnackPacket & { cmd: 'connack' };
+export type ConnectPacket = IConnectPacket & { cmd: 'connect' }
+export type ConnackPacket = IConnackPacket & { cmd: 'connack' }
 
-export type PubrelPacket = IPubrelPacket & { cmd: 'pubrel' };
-export type PingreqPacket = IPingreqPacket & { cmd: 'pingreq' };
+export type PubrelPacket = IPubrelPacket & { cmd: 'pubrel' }
+export type PingreqPacket = IPingreqPacket & { cmd: 'pingreq' }
 
-export type AedesPublishPacket = PublishPacket & AedesPacket;
+export type AedesPublishPacket = PublishPacket & AedesPacket
