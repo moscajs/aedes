9ec67649a75fd82aea8bb4b64010c4a0fb79332b
Hans Klunder <hans.klunder@gmail.com>
2025-06-30 19:12:34 +0000

    update benchmark server script

--- benchmarks/server.js
+++ benchmarks/server.js
@@ -2,24 +2,27 @@
 
 // To be used with cpuprofilify http://npm.im/cpuprofilify
 
-const aedes = require('../')()
-const server = require('net').createServer(aedes.handle)
-const port = 1883
+const { Aedes } = require('../')
 
-server.listen(port, function () {
-  console.error('server listening on port', port, 'pid', process.pid)
-})
+Aedes.createBroker().then(aedes => {
+  const server = require('net').createServer(aedes.handle)
+  const port = 1883
 
-aedes.on('clientError', function (client, err) {
-  console.error('client error', client.id, err.message)
-})
+  server.listen(port, function () {
+    console.error('server listening on port', port, 'pid', process.pid)
+  })
 
-// Cleanly shut down process on SIGTERM to ensure that perf-<pid>.map gets flushed
-process.on('SIGINT', onSIGINT)
+  aedes.on('clientError', function (client, err) {
+    console.error('client error', client.id, err.message)
+  })
 
-function onSIGINT () {
+  // Cleanly shut down process on SIGTERM to ensure that perf-<pid>.map gets flushed
+  process.on('SIGINT', onSIGINT)
+
+  function onSIGINT () {
   // IMPORTANT to log on stderr, to not clutter stdout which is purely for data, i.e. dtrace stacks
-  console.error('Caught SIGTERM, shutting down.')
-  server.close()
-  process.exit(0)
-}
+    console.error('Caught SIGTERM, shutting down.')
+    server.close()
+    process.exit(0)
+  }
+})
