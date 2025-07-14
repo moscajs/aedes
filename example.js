79824a503d6fce9cb6966f1539d54050d7741097
Hans Klunder <hans.klunder@gmail.com>
2025-07-14 12:59:39 +0200

    update example.js

--- example.js
+++ example.js
@@ -1,49 +1,57 @@
 'use strict'
 
-const aedes = require('./aedes')()
-const server = require('net').createServer(aedes.handle)
-const httpServer = require('http').createServer()
+const net = require('node:net')
+const http = require('node:http')
+const { Aedes } = require('./aedes')
 const ws = require('ws')
+
 const port = 1883
 const wsPort = 8888
 
-server.listen(port, function () {
-  console.log('server listening on port', port)
-})
+async function startAedes () {
+  const aedes = await Aedes.createBroker()
+  const server = net.createServer(aedes.handle)
+  const httpServer = http.createServer()
+  server.listen(port, function () {
+    console.log('server listening on port', port)
+  })
 
-const wss = new ws.WebSocketServer({
-  server
-})
+  const wss = new ws.WebSocketServer({
+    server: httpServer
+  })
 
-wss.on('connection', (websocket, req) => {
-  const stream = ws.createWebSocketStream(websocket)
-  aedes.handle(stream, req)
-})
+  wss.on('connection', (websocket, req) => {
+    const stream = ws.createWebSocketStream(websocket)
+    aedes.handle(stream, req)
+  })
 
-httpServer.listen(wsPort, function () {
-  console.log('websocket server listening on port', wsPort)
-})
+  httpServer.listen(wsPort, function () {
+    console.log('websocket server listening on port', wsPort)
+  })
 
-aedes.on('clientError', function (client, err) {
-  console.log('client error', client.id, err.message, err.stack)
-})
+  aedes.on('clientError', function (client, err) {
+    console.log('client error', client.id, err.message, err.stack)
+  })
 
-aedes.on('connectionError', function (client, err) {
-  console.log('client error', client, err.message, err.stack)
-})
+  aedes.on('connectionError', function (client, err) {
+    console.log('client error', client, err.message, err.stack)
+  })
 
-aedes.on('publish', function (packet, client) {
-  if (client) {
-    console.log('message from client', client.id)
-  }
-})
+  aedes.on('publish', function (packet, client) {
+    if (client) {
+      console.log('message from client', client.id)
+    }
+  })
 
-aedes.on('subscribe', function (subscriptions, client) {
-  if (client) {
-    console.log('subscribe from client', subscriptions, client.id)
-  }
-})
+  aedes.on('subscribe', function (subscriptions, client) {
+    if (client) {
+      console.log('subscribe from client', subscriptions, client.id)
+    }
+  })
 
-aedes.on('client', function (client) {
-  console.log('new client', client.id)
-})
+  aedes.on('client', function (client) {
+    console.log('new client', client.id)
+  })
+}
+
+startAedes()
