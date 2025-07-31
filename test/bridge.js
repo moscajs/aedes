9d438af547612bc149b6f8f9f0131c28ec7d203e
Hans Klunder <hans.klunder@gmail.com>
2025-07-31 18:46:55 +0200

    update bridge.js

--- test/bridge.js
+++ test/bridge.js
@@ -2,7 +2,8 @@ import { test } from 'node:test'
 import {
   createAndConnect,
   subscribe,
-  // checkNoPacket
+  checkNoPacket,
+  nextPacket
 } from './helperAsync.js'
 
 for (const qos of [0, 1, 2]) {
@@ -15,7 +16,7 @@ for (const qos of [0, 1, 2]) {
 
   if (qos > 0) packet.messageId = 42
 
-  test('normal client sends a publish message and shall receive it back, qos = ' + qos, async (t) => {
+  test(`normal client sends a publish message and shall receive it back, qos = ${qos}`, async (t) => {
     t.plan(4)
     const s = await createAndConnect(t)
     await subscribe(t, s, 'hello', qos)
@@ -32,16 +33,29 @@ for (const qos of [0, 1, 2]) {
     }
   })
 
-  // the next test will only work once mqtt-packet/writeToStream.js supports protocolVersion: 128 + 4
-  // TODO: fix mqtt-packet/writeToStream.js and then enable this test
-
-  // test('bridge client sends a publish message but shall not receive it back, qos = ' + qos, async (t) => {
-  //   t.plan(4)
-  //   // protocolVersion 128 + 4 means mqtt 3.1.1 with bridgeMode enabled
-  //   // https://github.com/mqttjs/mqtt-packet/blob/7f7c2ed8bcb4b2c582851d120a94e0b4a731f661/parser.js#L171
-  //   const s = await createAndConnect(t, { connect: { clientId: 'my-client-bridge-1', protocolVersion: 128 + 4 } })
-  //   await subscribe(t, s, 'hello', qos)
-  //   s.inStream.write(packet)
-  //   await checkNoPacket(t,s)
-  // })
+  test(`bridge client sends a publish message but shall not receive it back, qos = ${qos}`, async (t) => {
+    t.plan(6)
+    // protocolVersion 128 + 4 means mqtt 3.1.1 with bridgeMode enabled
+    // https://github.com/mqttjs/mqtt-packet/blob/7f7c2ed8bcb4b2c582851d120a94e0b4a731f661/parser.js#L171
+    const s = await createAndConnect(t, { connect: { clientId: 'my-client-bridge-1', bridgeMode: true } })
+    await subscribe(t, s, 'hello', qos)
+    s.inStream.write(packet)
+    if (qos === 0) {
+      t.assert.ok(true) // need two extra asserts to match QoS2
+      t.assert.ok(true) // need two extra asserts to match QoS2
+    }
+    if (qos === 1) {
+      const pkt = await nextPacket(s)
+      t.assert.equal(pkt.cmd, 'puback', 'received QoS1 puback')
+      t.assert.ok(true) // need an extra assert to match QoS2
+    }
+    if (qos === 2) {
+      const pkt = await nextPacket(s)
+      t.assert.equal(pkt.cmd, 'pubrec', 'received QoS2 pubRec')
+      s.inStream.write({ cmd: 'pubrel', messageId: 42 })
+      const pkt2 = await nextPacket(s)
+      t.assert.equal(pkt2.cmd, 'pubcomp', 'received QoS2 pubcomp')
+    }
+    await checkNoPacket(t, s)
+  })
 }
