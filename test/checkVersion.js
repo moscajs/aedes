2a9e21ea728dd25581d3a3495ba0d48dfa53e3b0
Hans Klunder <hans.klunder@gmail.com>
2025-08-07 23:30:41 +0200

    6th update to unit test def for node 20

--- test/checkVersion.js
+++
@@ -1,8 +0,0 @@
-// node 20 on windows has issues with node:test wildcards
-import { platform, version } from 'node:process'
-const major = version.split('.')[0]
-console.log(`Running ${major} on ${platform}`)
-if ((major === 'v20') || platform === 'win32') {
-  process.exit(0)
-}
-process.exit(1)
